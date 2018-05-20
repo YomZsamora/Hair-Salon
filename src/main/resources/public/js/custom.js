ac8160dab45264362666241e27a711ea0d3f4de0
yomZsamora <samora.y@adzumi.co.ke>
2018-05-20 09:48:39 +0300

    Added form validation functionality in update stylist page.

--- src/main/resources/public/js/custom.js
+++ src/main/resources/public/js/custom.js
@@ -20,4 +20,39 @@ jQuery(document).ready(function() {
    jQuery('#toggleBackUpdateClient').click(function(){
       $('#updateClientForm').hide();
    });
+
+   $("form[name='addStylist']").validate({
+      // Specify validation rules
+      rules: {
+         // The key name on the left side is the name attribute
+         // of an input field. Validation rules are defined
+         // on the right side
+         stylistFirstName: "required",
+         stylistLastName: "required",
+         stylistAge: {
+            required: true,
+            number: true
+         },
+         stylistPhone: "required",
+         stylistDept: "required"
+      },
+      // Specify validation error messages
+      messages: {
+         stylistFirstName: "Please update stylist's first name.",
+         stylistLastName: "Please update stylist's last name.",
+         stylistAge: {
+            required: "Please update stylist's age.",
+            number: "Please Enter a Valid Number."
+         },
+         stylistPhone: "Please update stylist's phone number.",
+         stylistDept: "Please update stylist's departmant."
+      },
+      // Make sure the form is submitted to the destination defined
+      // in the "action" attribute of the form when valid
+      submitHandler: function(form) {
+      form.submit();
+    }
+  });
+
+
 });
